a60c78e43630832dc9f700b59cdc59357c0fb4d5
Zoe <blink_zoe@hotmail.com>
2026-07-06 10:53:30 -0600

    NEW Project

--- src/services/security2fa.ts
+++
@@ -1,9 +0,0 @@
-import { authenticator } from 'otplib';
-
-export function generarCodigo2FA(semillaBase32: string): string {
-  try {
-    return authenticator.generate(semillaBase32);
-  } catch (error) {
-    throw new Error('Error generando el token de seguridad TOTP.');
-  }
-}
